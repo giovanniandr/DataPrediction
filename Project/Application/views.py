1b4f134562970c75988f6c564be8782d5d12f472
giovanniandr <fs_giovanni@hotmail.com>
2022-04-20 13:07:46 +0000

    Before superuser

--- Project/Application/views.py
+++ Project/Application/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+import folium
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    #Creating map
+    m = folium.Map()
+    m = m._repr_html_()
+    context={
+        'm': m,
+    }
+
+    #Returning a render of index.html
+    return render(request, 'index.html', context)
