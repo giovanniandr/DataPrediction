14b7e1d4f804057e1f72ef5a34206b3978a0931f
giovanniandr <fs_giovanni@hotmail.com>
2022-04-27 15:43:18 +0000

    Dashboard concept

--- Project/static/main.js
+++ Project/static/main.js
@@ -1,15 +1,38 @@
 const apiKey = 'pk.eyJ1IjoiZ2lvdmFubmlhbmRyIiwiYSI6ImNsMWtucG02ZDAyNTIzaW8zeWg1aG5qbmwifQ.yOWlVopRvptHXp4Kp-3v4A'
 
-const map = L.map('map').setView([51.505, -0.09], 13);
+const map = L.map('map', { zoomControl:false, attributionControl:false }).setView([53.346, -6.254], 12);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    
     maxZoom: 18,
-    id: 'mapbox/streets-v11',
+    id: 'giovanniandr/cl2gbpvbt003214kskxixuz4y',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: apiKey
 }).addTo(map);
+
+//Read csv and get auto latitude and longitude from listings.csv
+const markers = L.marker([53.29178, -6.25792]).addTo(map);
+
+//Message
+let template = `
+
+<p> Cosy comfortable Irish cottage twin </p>
+<p> Room type: Private Room </p>
+<p> Neighbourhood: Dn Laoghaire-Rathdown </p>
+`
+
+
+// Read csv and get property name for each property
+markers.bindPopup(template).openPopup();
+
+
+
+
+
+
+
+
 
 
 
--- README.md
+++ README.md
@@ -8,6 +8,9 @@ To install gdal on gitpod https://github.com/OSGeo/gdal/issues/5442
 pip install Django
 pip install geocoder
 pip install folium
-pip install GDAL
+pip install django-leaflet
+sudo add-apt-repository -y ppa:ubuntugis/ubuntugis-unstable
+sudo apt-get update
+sudo apt-get install gdal-bin libgdal-dev
 
 python manage.py runserver
