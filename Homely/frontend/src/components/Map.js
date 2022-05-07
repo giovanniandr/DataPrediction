2d5943616c24c0e1231183a7262a393ea2c2b24d
giovanniandr <fs_giovanni@hotmail.com>
2022-05-07 20:13:07 +0000

    React components

---
+++ Homely/frontend/src/components/Map.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Map extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            lon:
+            lat:
+        }
+    }
+}
+
+export default Map
--- Homely/frontend/src/components/Navbar/Navbar.js
+++ Homely/frontend/src/components/Navbar/Navbar.js
@@ -1,32 +1,46 @@
 import React, { Component } from "react";
 import { MenuItems } from "./MenuItems";
 import "./Navbar.css"
-
+import logo from "../../logo.png";
 
 class Navbar extends Component {
+
     render () {
         return (
-            <nav className="nav"> 
-            <div className="logo">
-                
+            <div className="menu"> 
+                <div className="logo">
+                    <img src={ logo } alt="Homely Logo"/>
+                </div>
+
+                <div className="search">
+                    <div className="input-group stylish-input-group">
+                        <span className="input-group-addon"> 
+                            <button type="submit"> 
+                                <span class="glyphicon glyphicon-search"></span>
+                            </button>
+                        </span>
+                        <input type="text" className="form-control" placeholder="Search for the location within Dublin, Ireland" ></input>
+                    </div>
+                </div>
+
+                <nav className="nav"> 
+                <div className="wrap">
+                <ul className="nav-menu" >
+                {MenuItems.map((item, index) =>{
+                    return (
+                    <li className="nav-item" key={index}>
+                        <a className={item.cName} class="nav-link"  href={item.url}> 
+                        {item.title}     
+                        </a>
+                    </li>
+
+                    )
+                })}
+                </ul>
             </div>
-
-            <div class="wrap">
-            <ul class="navbar-nav mr-auto" >
-              {MenuItems.map((item, index) =>{
-                return (
-                  <li class="nav-item active" key={index}>
-                    <a className={item.cName} class="nav-item is-active"  href={item.url}> 
-                      {item.title}     
-                    </a>
-                  </li>
-
-                )
-              })}
-            </ul>
-          </div>
-          <span class="nav-indicator"></span>
-        </nav>
+            <span className="nav-indicator"></span>
+            </nav>
+        </div>
         )
     }
 }
