841e0b35883b3d8ceca37d2a24cce2828d0580e4
giovanniandr <fs_giovanni@hotmail.com>
2022-05-10 15:28:25 +0000

    React clustering in progress

--- Homely/frontend/src/App.js
+++ Homely/frontend/src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
 import Navbar from "./components/Navbar/Navbar";
+import Map from "./components/Map/Map";
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Map />
     </div>
   );
 }
+
+
 
 export default App;
