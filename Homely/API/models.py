ac54c79c1dd98c56c4ef1641cec24511892d4102
giovanniandr <fs_giovanni@hotmail.com>
2022-05-10 13:27:58 +0000

    Correction

--- Homely/API/models.py
+++ Homely/API/models.py
@@ -1,18 +1,17 @@
 from django.db import models
-import geocoder
 
 # Create your models here.
 ##Ignore this model as it is not perfect
 token='pk.eyJ1IjoiZ2lvdmFubmlhbmRyIiwiYSI6ImNsMWtucG02ZDAyNTIzaW8zeWg1aG5qbmwifQ.yOWlVopRvptHXp4Kp-3v4A'
 
 class Address(models.Model):
-    address = models.TextField()
+    neighbourhood = models.TextField()
+    city = models.TextField()
+    state = models.TextField()
+    property = models.TextField()
+    bathrooms = models.DecimalField(max_digits=10, decimal_places=2)
+    bedrooms = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     lat = models.FloatField(blank = True, null=True)
     long = models.FloatField(blank = True, null=True)
 
-def save(self, *args, **kwargs):
-    g = geocoder.mapbox(self.address, key='TOKEN')
-    g = g.latlng 
-    self.lat = g[0]
-    selt.long = g[1]
-    return super (Address, self).save(*args, **kwargs)
